8c863d63772701dfc282113b1ba0f56b9f33ecae
Eduardo Zamora <eduzambl@gmail.com>
2022-06-16 15:52:16 -0600

    feat: taks1 add reset state reducer test

---
+++ src/reducers/game/__tests__/reset.ts
@@ -0,0 +1,27 @@
+import { BoardState } from "../board";
+import { getInitialBoard } from "../getInitialBoard";
+import { reducer } from "../index";
+
+const initialState: BoardState = getInitialBoard();
+
+describe("test game reset", () => {
+  test("returns the initialState if game is resetted", () => {
+    expect(
+      reducer(
+        {
+          board: [
+            [null, null, null, null, null, "yellow", null],
+            [null, null, null, null, null, "red", null],
+            [null, null, null, null, null, "yellow", null],
+            [null, null, null, null, null, "red", null],
+            [null, null, null, null, null, "yellow", null],
+            [null, null, null, null, null, "red", null],
+          ],
+        },
+        {
+          type: "RESET",
+        }
+      )
+    ).toEqual({ board: initialState });
+  });
+});
